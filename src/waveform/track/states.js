5eba03660b17528d02dd60ebe3bcf13612f0c220
Naomi Aro <naomiaro@gmail.com>
2015-11-26 01:14:23 -0800

    Skeleton for planning track states.

---
+++ src/waveform/track/states.js
@@ -0,0 +1,16 @@
+import cursor from "states/cursor";
+import fadein from "states/fadein";
+import fadeout from "states/fadeout";
+import record from "states/record";
+import select from "states/select";
+import shift from "states/shift";
+
+//export an object holding all states.
+export default {
+	cursor,
+	fadein,
+	fadeout,
+	record,
+	select,
+	shift
+}
