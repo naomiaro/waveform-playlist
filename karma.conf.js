9335df35142517b22f9c948071d75866aaeabff9
Naomi Aro <naomiaro@gmail.com>
2015-11-27 16:32:49 -0800

    Still working on setting up Karma.

--- karma.conf.js
+++ karma.conf.js
@@ -10,14 +10,14 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'sinon-chai'],
+    frameworks: ['mocha', 'requirejs', 'chai-as-promised', 'chai'],
 
 
     // list of files / patterns to load in the browser
     files: [
       'test-main.js',
       {pattern: 'src/**/*.js', included: false},
-      {pattern: 'test/**/*Spec.js', included: false}
+      {pattern: 'test/**/*.spec.js', included: false}
     ],
 
 
@@ -31,7 +31,7 @@ module.exports = function(config) {
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       'src/**/*.js': ['babel'],
-      'test/**/*Spec.js': ['babel']
+      'test/**/*.spec.js': ['babel']
     },
 
     babelPreprocessor: {
@@ -51,7 +51,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['nyan'],
 
 
     // web server port
@@ -64,7 +64,7 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
 
 
     // enable / disable watching file and executing tests whenever any file changes
@@ -74,7 +74,7 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     //browsers: ['Chrome', 'Firefox'],
-    browsers: [],
+    browsers: ['Chrome'],
 
 
     // Continuous Integration mode
