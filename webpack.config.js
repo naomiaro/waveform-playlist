399510474a8ffb17ea7795e7b3d730cdef8564d9
Naomi Aro <naomiaro@gmail.com>
2016-05-22 00:44:28 +0100

    Don't bother with parallel web pack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,18 @@
-var webpack = require("webpack");
-var createVariants = require('parallel-webpack').createVariants;
-
-function createConfig(options) {
-  var plugins = [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.DedupePlugin()
-  ];
-  if (options.minified) {
-    plugins.push(new webpack.optimize.UglifyJsPlugin({
-      sourceMap: false,
-      compress: {
-        warnings: false
-      }
-    }));
+module.exports = {
+  entry: __dirname + "/src/app.js",
+  output: {
+    path:  __dirname + "/dist/js",
+    publicPath: "js/",
+    filename: 'waveform-playlist.var.js',
+    library: 'WaveformPlaylist',
+    libraryTarget: 'var'
+  },
+  devtool: "#source-map",
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
   }
-
-  return {
-    entry: __dirname + "/src/app.js",
-    output: {
-      path:  __dirname + "/dist/js",
-      publicPath: "js/",
-      filename: 'waveform-playlist.' +
-        options.target +
-        (options.minified ? '.min' : '')
-        + '.js',
-      library: 'WaveformPlaylist',
-      libraryTarget: options.target
-    },
-    module: {
-      loaders: [{
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      }]
-    },
-    plugins: plugins
-  };
-}
-
-module.exports = createVariants({
-  minified: [true, false],
-  target: ['var']
-}, createConfig);
+};
