60f59e2d9e79315e0cf793a0af3cbfaaef08eb17
Naomi Aro <naomiaro@gmail.com>
2021-04-24 15:18:43 -0700

    set gain on stems to 1

--- ghpages/js/stem-tracks.js
+++ ghpages/js/stem-tracks.js
@@ -3,38 +3,36 @@ var playlist = WaveformPlaylist.init({
   waveHeight: 100,
   container: document.getElementById("playlist"),
   timescale: true,
-  state: 'cursor',
+  state: "cursor",
   colors: {
-    waveOutlineColor: '#E0EFF1'
+    waveOutlineColor: "#E0EFF1",
   },
   controls: {
     show: true, //whether or not to include the track controls
-    width: 200 //width of controls in pixels
+    width: 200, //width of controls in pixels
   },
-  zoomLevels: [500, 1000, 3000, 5000]
+  zoomLevels: [500, 1000, 3000, 5000],
 });
 
-playlist.load([
-  {
-    "src": "media/audio/Vocals30.mp3",
-    "name": "Vocals",
-    "gain": 0.75,
-    "muted": false,
-    "soloed": false
-  },
-  {
-    "src": "media/audio/Guitar30.mp3",
-    "name": "Guitar"
-  },
-  {
-    "src": "media/audio/PianoSynth30.mp3",
-    "name": "Pianos & Synth",
-    "gain": 1
-  },
-  {
-    "src": "media/audio/BassDrums30.mp3",
-    "name": "Drums"
-  }
-]).then(function() {
-  //can do stuff with the playlist.
-});
+playlist
+  .load([
+    {
+      src: "media/audio/Vocals30.mp3",
+      name: "Vocals",
+    },
+    {
+      src: "media/audio/Guitar30.mp3",
+      name: "Guitar",
+    },
+    {
+      src: "media/audio/PianoSynth30.mp3",
+      name: "Pianos & Synth",
+    },
+    {
+      src: "media/audio/BassDrums30.mp3",
+      name: "Drums",
+    },
+  ])
+  .then(function () {
+    //can do stuff with the playlist.
+  });
--- src/render/CanvasHook.js
+++ src/render/CanvasHook.js
@@ -35,7 +35,7 @@ class CanvasHook {
 
     const scale = this.scale;
     const len = canvas.width / scale;
-    const cc = canvas.getContext("2d");
+    const cc = canvas.getContext('2d');
     const h2 = canvas.height / scale / 2;
     const maxValue = 2 ** (this.bits - 1);
 
