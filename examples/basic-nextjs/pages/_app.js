8b179f35ddfc03d17d5eb792e40e2d3841a6f29b
Naomi Aro <naomiaro@gmail.com>
2022-02-10 21:07:50 -0800

    very basic nextjs

---
+++ examples/basic-nextjs/pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.css'
+import 'waveform-playlist/styles/playlist.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ examples/basic-nextjs/pages/index.js
@@ -0,0 +1,55 @@
+import React, {useCallback, useState} from 'react';
+
+import WaveformPlaylist from 'waveform-playlist';
+
+const Waveform = () => {
+  const [playlist, setPlaylist] = useState(null);
+
+  const container = useCallback(node => {
+    if (node !== null) {
+      const playlist = WaveformPlaylist({
+        samplesPerPixel: 100,
+        mono: true,
+        waveHeight: 70,
+        container: node,
+        state: "cursor",
+        colors: {
+          waveOutlineColor: "#E0EFF1",
+          timeColor: "grey",
+          fadeColor: "black",
+        },
+        controls: {
+          show: false,
+          width: 150,
+        },
+        zoomLevels: [100, 300, 500],
+      });
+  
+      playlist.load([
+        {
+          src: "hello.mp3",
+          name: "Hello",
+        }
+      ])
+      .then(function () {
+        // can do stuff with the playlist.
+      });
+
+      setPlaylist(playlist);
+    }
+  }, []);
+
+
+  return (
+    <main>
+      <div><button onClick={() => { playlist.getEventEmitter().emit("play") }}>Play</button></div>
+      <div ref={container}></div>
+    </main>
+  );
+};
+
+const Playlist = () => {
+  return (<Waveform></Waveform>)
+};
+
+export default Playlist;
