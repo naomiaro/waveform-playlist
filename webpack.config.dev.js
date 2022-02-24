fd821087d8b9f55466cb0c95579d833f75ad7668
Naomi Aro <naomiaro@gmail.com>
2022-02-23 23:26:10 -0800

    better setup

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -16,4 +16,19 @@ module.exports = {
   },
   mode: "development",
   devtool: "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [["@babel/plugin-transform-runtime"]],
+          },
+        },
+      },
+    ],
+  },
 };
--- webpack.config.js
+++ webpack.config.js
@@ -15,4 +15,19 @@ module.exports = {
     },
   },
   mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [["@babel/plugin-transform-runtime"]],
+          },
+        },
+      },
+    ],
+  },
 };
