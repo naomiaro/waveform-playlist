87f15921056d37ea0711a8b8a15569847d5abaf8
markus <markus.larsson@wearelevels.com>
2022-05-24 13:50:34 +0200

    removed time markers

--- src/TimeScale.js
+++ src/TimeScale.js
@@ -1,6 +1,6 @@
 import h from "virtual-dom/h";
 
-import { secondsToPixels } from "./utils/conversions";
+import {secondsToPixels} from "./utils/conversions";
 import TimeScaleHook from "./render/TimeScaleHook";
 
 class TimeScale {
@@ -131,7 +131,6 @@ class TimeScale {
               [TimeScale.formatTime(counter)]
             )
           );
-
           canvasInfo[pix] = 10;
         } else if (scaleInfo.bigStep && counter % scaleInfo.bigStep === 0) {
           canvasInfo[pix] = 5;
@@ -151,21 +150,7 @@ class TimeScale {
         },
       },
       [
-        timeMarkers,
-        h("canvas", {
-          attributes: {
-            width: widthX,
-            height: 30,
-            style: "position: absolute; left: 0; right: 0; top: 0; bottom: 0;",
-          },
-          hook: new TimeScaleHook(
-            canvasInfo,
-            this.offset,
-            this.samplesPerPixel,
-            this.duration,
-            this.colors
-          ),
-        }),
+        timeMarkers
       ]
     );
   }
