c3e0d2b3bc48aba58b84e58cbd906c9ff24fca96
linomp <lm2p@hotmail.es>
2022-08-17 12:42:48 +0200

    initial version - download btn & cleanup

--- examples/basic-express/routes/track.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET track page. */
-router.get('/', function(req, res, next) {
-  res.render('track', { title: 'Load a track' });
-});
-
-module.exports = router;
--- src/track/states/RecordState.js
+++
@@ -1,9 +0,0 @@
-export default class {
-  constructor(track) {
-    this.track = track;
-  }
-
-  static getClass() {
-    return ".state-record";
-  }
-}
