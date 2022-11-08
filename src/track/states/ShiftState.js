300906040736f69f2410045f19d59ec78984f113
Vinicius Gusmão <vinicius-og@hotmail.com>
2022-11-08 09:18:12 -0300

    Shift feature mobile adapt

--- src/track/states/ShiftState.js
+++ src/track/states/ShiftState.js
@@ -11,6 +11,11 @@ export default class {
     this.sampleRate = sampleRate;
   }
 
+  getXOffsetOnTouchEvent(e) {
+    const bcr = e.target.getBoundingClientRect();
+    return e.targetTouches[0].clientX - bcr.x;
+  }
+
   emitShift(x) {
     const deltaX = x - this.prevX;
     const deltaTime = pixelsToSeconds(
@@ -35,10 +40,25 @@ export default class {
     this.prevX = e.offsetX;
   }
 
+  touchstart(e) {
+    e.preventDefault();
+
+    this.active = true;
+    this.el = e.target;
+    this.prevX = this.getXOffsetOnTouchEvent(e);
+  }
+
   mousemove(e) {
     if (this.active) {
       e.preventDefault();
       this.emitShift(e.offsetX);
+    }
+  }
+
+  touchmove(e) {
+    if (this.active) {
+      e.preventDefault();
+      this.emitShift(this.getXOffsetOnTouchEvent(e));
     }
   }
 
@@ -56,11 +76,26 @@ export default class {
     }
   }
 
+  touchend(e) {
+    if (this.active) {
+      e.preventDefault();
+      this.complete(this.getXOffsetOnTouchEvent(e));
+    }
+  }
+
   static getClass() {
     return ".state-shift";
   }
 
   static getEvents() {
-    return ["mousedown", "mousemove", "mouseup", "mouseleave"];
+    return [
+      "mousedown",
+      "mousemove",
+      "mouseup",
+      "mouseleave",
+      "touchstart",
+      "touchmove",
+      "touchend",
+    ];
   }
 }
